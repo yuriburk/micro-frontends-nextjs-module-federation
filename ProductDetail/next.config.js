d0af925aca0ccfddd745ca1003e7055f357bd3dd
Yuri Burk <yuriburk@gmail.com>
2022-02-04 22:02:54 -0300

    withFederatedSidecar

--- ProductDetail/next.config.js
+++ ProductDetail/next.config.js
@@ -1,35 +1,17 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withModuleFederation } = require('@module-federation/nextjs-mf')
+const { withFederatedSidecar } = require('@module-federation/nextjs-mf')
 
-module.exports = {
-  webpack5: true,
-  webpack: (config, options) => {
-    const { isServer } = options
-    const mfConf = {
-      mergeRuntime: true, //experimental
-      name: 'productDetail',
-      library: {
-        type: config.output.libraryTarget,
-        name: 'productDetail'
-      },
-      filename: 'static/runtime/productDetailRemoteEntry.js',
-      remotes: {},
-      exposes: {
-        './main': './src/components/Main'
-      }
-    }
-    config.cache = false
-    withModuleFederation(config, options, mfConf)
-    if (!isServer) {
-      config.output.publicPath = 'http://localhost:3001/_next/'
-    }
-
-    return config
+module.exports = withFederatedSidecar({
+  name: 'productDetail',
+  filename: 'static/chunks/remoteEntry.js',
+  exposes: {
+    './main': './src/components/Main'
   },
-
-  webpackDevMiddleware: (config) => {
-    // Perform customizations to webpack dev middleware config
-    // Important: return the modified config
-    return config
+  shared: {
+    react: {
+      // Notice shared are NOT eager here.
+      requiredVersion: false,
+      singleton: true
+    }
   }
-}
+})({})
--- Shell/next.config.js
+++ Shell/next.config.js
@@ -1,30 +1,32 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const { withModuleFederation } = require('@module-federation/nextjs-mf')
+const ExternalTemplateRemotesPlugin = require('external-remotes-plugin')
 
 module.exports = {
-  webpack5: true,
-  webpack: (config, options) => {
-    const mfConf = {
-      name: 'shell',
-      library: {
-        type: config.output.libraryTarget,
-        name: 'shell'
-      },
-      remotes: {
-        productsList: 'productsList',
-        productDetail: 'productDetail'
-      },
-      exposes: {}
-    }
-    config.cache = false
-    withModuleFederation(config, options, mfConf)
+  webpack(config, options) {
+    config.plugins.push(
+      new options.webpack.container.ModuleFederationPlugin({
+        remoteType: 'var',
+        remotes: {
+          productsList: 'productsList',
+          productDetail: 'productDetail@http://localhost:3001/remoteEntry.js'
+        },
+        shared: {
+          react: {
+            // Notice shared ARE eager here.
+            eager: true,
+            singleton: true,
+            requiredVersion: false
+          }
+        }
+      })
+    )
+    config.plugins.push(new ExternalTemplateRemotesPlugin())
 
-    return config
-  },
+    config.module.rules.push({
+      test: /_app.tsx/,
+      loader: '@module-federation/nextjs-mf/lib/federation-loader.js'
+    })
 
-  webpackDevMiddleware: (config) => {
-    // Perform customizations to webpack dev middleware config
-    // Important: return the modified config
     return config
   }
 }
